80114a11c31da551e0183e383a96ab37cf430d4a
anirban <Anirban20001962>
2021-01-08 20:37:20 +0530

    second9 production

--- app.js
+++ app.js
@@ -39,12 +39,10 @@ app.use(multer({ storage: fileStorage,fileFilter: fileFilter}).single('image'))
 app.use('/images', express.static(path.join(__dirname,'images')))
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,PATCH,DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, OTP");
     next();
-    
 })
 /*app.use(cors())*/
 
