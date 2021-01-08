154d4b6e4d96679ce2bff56359f22c4acfc85d0a
anirban <Anirban20001962>
2021-01-08 20:24:25 +0530

    second8 production

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer');
 const { mongodb_url } = require('./config');
-const cors = require('cors');
 
 //let fileStorage = multer.memoryStorage()
 
@@ -40,12 +39,12 @@ app.use(multer({ storage: fileStorage,fileFilter: fileFilter}).single('image'))
 app.use('/images', express.static(path.join(__dirname,'images')))
 
 app.use((req, res, next) => {
-    res.setHeader('Accept', 'application/json');
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type,  Authorization");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
     next();
+    
 })
 /*app.use(cors())*/
 
